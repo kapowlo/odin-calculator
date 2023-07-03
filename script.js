774c50698dbec11de1fbaa2c03e094ee8faa49dd
mano <rsmike91@outlook.com>
2023-07-03 16:58:45 -0400

    Added four functions for basic math operations
    Added operate() function
    this func will call add,subtract,multiply or divide
    based on the operator sign + - * /

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 
+function add(array){
+    let num1=Number(array[0]);//grab the first number
+    let op=array[1]//grab the operator
+    let num2=Number(array[2]);//grab the second number
+    if(op==="+"){
+        return num1 + num2; 
+    }
+}
+function subtract(array){
+    let num1=Number(array[0]);//grab the first number
+    let op=array[1]//grab the operator
+    let num2=Number(array[2]);//grab the second number
+    if(op==="-"){
+        return num1 - num2; 
+    }
+}
+
+function multiply(array){
+    let num1=Number(array[0]);//grab the first number
+    let op=array[1]//grab the operator
+    let num2=Number(array[2]);//grab the second number
+    if(op==="*"){
+        return num1 * num2; 
+    }
+}
+function divide(array){
+    let num1=Number(array[0]);//grab the first number
+    let op=array[1]//grab the operator
+    let num2=Number(array[2]);//grab the second number
+    if(op==="/"){
+        return num1 / num2; 
+    }
+}
+
+function operate(numberOne,operator,numberTwo){
+    // this function will call add,subtract,multiply,divide func depending on the operator
+    const arr=[numberOne,operator,numberTwo]; // make an array [num,"+ - * /",num ]
+        if(operator==="+"){
+            let sum=add(arr) //pass the arr in func call
+            return sum
+        }
+        else if(operator==="-"){
+            let subtraction=subtract(arr)
+            return subtraction
+        }
+        else if(operator==="*"){
+            let multiplication=multiply(arr)
+            return multiplication
+        }
+        else if(operator==="/"){
+            let division=divide(arr)
+            return division
+        }
+        else{
+            return `invalid operator`
+        }
+}
