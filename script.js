ff60c13f48e34ef1932560a6970d8c27c930fd58
mano <rsmike91@outlook.com>
2023-07-19 21:19:13 -0400

    Massive changes to the codebase simplified the code by adding functions while removing all four arithmetic functions: add,subtract,multiply,divide
    Removed all lines that created elements via javascript
    instead I created the different elements inside html file

--- script.js
+++ script.js
@@ -1,138 +1,121 @@
 
-function add(array){
-    let num1=Number(array[0]);//grab the first number
-    let op=array[1]//grab the operator
-    let num2=Number(array[2]);//grab the second number
-    if(op==="+"){
-        return num1 + num2; 
-    }
-}
-function subtract(array){
-    let num1=Number(array[0]);//grab the first number
-    let op=array[1]//grab the operator
-    let num2=Number(array[2]);//grab the second number
-    if(op==="-"){
-        return num1 - num2; 
-    }
-}
-
-function multiply(array){
-    let num1=Number(array[0]);//grab the first number
-    let op=array[1]//grab the operator
-    let num2=Number(array[2]);//grab the second number
-    if(op==="*"){
-        return num1 * num2; 
-    }
-}
-function divide(array){
-    let num1=Number(array[0]);//grab the first number
-    let op=array[1]//grab the operator
-    let num2=Number(array[2]);//grab the second number
-    if(op==="/"){
-        return num1 / num2; 
-    }
-}
-
-function operate(numberOne,operator,numberTwo,equalSign){
-    // this function will call add,subtract,multiply,divide func depending on the operator
-    const arr=[numberOne,operator,numberTwo,equalSign]; // make an array [num,"+ - * / =",num ]
-        if(operator==="+"){
-            let sum=add(arr) //pass the arr that contains the numbers in func call
-            return sum
-        }
-        else if(operator==="-"){
-            let subtraction=subtract(arr)
-            return subtraction
-        }
-        else if(operator==="*"){
-            let multiplication=multiply(arr)
-            return multiplication
-        }
-        else if(operator==="/"){
-            let division=divide(arr)
-            return division
-        }
-        else{
-            return `invalid operator`
-        }
-        
-}
-//these paragraphs will hold the numbers and the operator once a btn is clicked
-// MUST append all three paragraphs to container
-const body=document.body;
-
-let firstNumberPara=document.createElement("p");
-let operatorPara=document.createElement("p"); // for the + - * / arithmetic signs
-let secondNumberPara=document.createElement("p");
-let storeEqualSignPara=document.createElement("p");// paragraph to store the equal sign
+let operatorValue="";
+let currentNumberValue="";
+let previousNumberValue=""
 
 
-const displayPara=document.querySelector(".display-screen");
-displayPara.append(firstNumberPara,operatorPara,secondNumberPara,storeEqualSignPara);
+//select all the buttons with class number
+const numbersButton=document.querySelectorAll(".number");
 
-// create loop for the buttons then add click event for the buttons 
-//the first paragraph will be equal to the text content of the button that was clicked
-// after the user clicks once that number gets stored in the first para, when the user click a second time on the operators
-//like + - * / the operator para will be equal to the text content  of the button that was click + - * /
-//then user clicks on a second number and that number will be stored in the secondNumberPara
-//finally when user click on equal sign we will display the result on screen
-const myButtons=document.querySelectorAll(".container button");
-let step=1;
-myButtons.forEach(btn=>{
-    
-    btn.addEventListener("click",event=>{
-        switch(step){
-            case 1:
-                firstNumberPara.textContent+=event.target.textContent ; 
-                step=2;
-                break;
-            case 2:
-                operatorPara.textContent=event.target.textContent;
-                step=3;
-                break;
-            case 3:
-                secondNumberPara.textContent+=event.target.textContent ;
-                step=4;
-                break;
-            case 4:
-                storeEqualSignPara.textContent=event.target.textContent
-                break;
-        }  
-    });
-});
+//select all  the buttons with class operator
+const operatorsButton=document.querySelectorAll(".operator");
 
-//Moved the operationBtn outside of the forEach loop, because the eventlistener was added multiple times
-const operationBtn=document.querySelector(".operation");
-    operationBtn.addEventListener("click",result=>{
-         if (step === 4) {
-    storeEqualSignPara.textContent = "=";
-    const result = operate(firstNumberPara.textContent, operatorPara.textContent, secondNumberPara.textContent, "=");
-    Array.from(displayPara.children).forEach(para => {
-      para.remove();
-    });
-    displayPara.append(result);
-  }
-    });
-//remove paragraphs when clear button is pressed
-//currently doesn't work, as it permanently removes the paragraphs
-const clearBtn=document.querySelector(".clear");
-clearBtn.addEventListener("click",()=>{
-    Array.from(displayPara.children).forEach(para=>{ 
-        para.remove(); // removes the  paragraphs, but it doesn't seem to work after I press the equal sign
-       });
-       displayPara.textContent="" // removes the result after pressing equal sign
-       
-      
-       remakePara();// remake all of the paragraphs that I removed
-       step=1; //ensures that the code starts from the beginning when the user interacts with the calculator again
-       
+// select the paragraphs inside of display-screen div
+const currentNumberPara = document.querySelector(".currentNumber")
+const previousNumberPara = document.querySelector(".previousNumber")
+
+
+//select the remaining buttons
+const clearButton=document.querySelector(".clear")
+const equalButton=document.querySelector(".equal")
+const decimalButton=document.querySelector(".decimal") // try  to implement it 
+
+// create eventlistener for number buttons
+//the currentNumberPara element inside of display-screen div will be equal to the button's text content
+numbersButton.forEach(button=>{
+    button.addEventListener("click",(e)=>{
+        getNumber(e.target.textContent); // call func and pass the button's text content to it
+        currentNumberPara.textContent=currentNumberValue
+        //currentNumberValue=currentNumberPara.textContent;
+        //previousNumberPara.textContent=currentNumberValue;
+    })
 })
-function remakePara(){
-    firstNumberPara=document.createElement("p");
-    operatorPara=document.createElement("p"); // for the + - * / arithmetic signs
-    secondNumberPara=document.createElement("p");
-    storeEqualSignPara=document.createElement("p");// paragraph to store the equal sign
+
+
+// create eventlistener for operator buttons
+operatorsButton.forEach(opButton=>{
+    opButton.addEventListener("click",(e)=>{
+    // call func and pass the button's text content to it,the func will switch  value of previousNumberValue to equal currentNumberValue
+        getOperator(e.target.textContent)
+    //now that previousNumberValue contains the currentNumberValue, I can store that inside of previousNumberParagraph along with operator
+        previousNumberPara.textContent=previousNumberValue + " " + operatorValue; 
+        // currentNumberValue is now an empty string
+        currentNumberPara.textContent=currentNumberValue;
+    })
+})
+
+
+
+// create event listener for clear button
+//when this button is clicked 
+//call func that will set currentNumberPara/previousNumberPara/operatorValue to empty strings and undefined
+clearButton.addEventListener("click",()=>{
+    resetCalc();
+    previousNumberPara.textContent=previousNumberValue;
+    currentNumberPara.textContent=currentNumberValue;
+})
+
+
+
+// create event listener for equal button
+// when this button is clicked call compute() or calculate() function
+// this function will take one param and check for the operator sign and do the appropriate operation + - * /
+equalButton.addEventListener("click",()=>{
+    getResult()
+    previousNumberPara.textContent="";
+    currentNumberPara.textContent=previousNumberValue;
+  
+})
+
+decimalButton.addEventListener("click",()=>{
+    if(!currentNumberValue.includes(".")){ // if it does'nt include . then add one
+        currentNumberValue +="."
+    }
+})
+
+
+function getNumber(num){
+    //call when user click on number btn
+    //stores a number inside currentNumber variable then returns the number
+    //also allows user to  input large numbers like 2987112
+    currentNumberValue+=num;
     
-    displayPara.append(firstNumberPara,operatorPara,secondNumberPara,storeEqualSignPara) 
+}
+function getOperator(op){
+    //call when user clicks on operator btn
+    //store operator sign inside operatorValue
+    // when user clicks on any of the 4 op buttons, the previousNumberValue will take the value of the currentNumbervalue
+    //then i'll reset the currentNumberValue to an empty string
+    operatorValue=op;
+    previousNumberValue=currentNumberValue;
+    currentNumberValue=""
+}
+
+function resetCalc(){
+    //call when user click on clear btn
+    //makes the display-screen div completely blank
+    operatorValue=undefined;
+    currentNumberValue="";
+    previousNumberValue=""
+}
+
+function getResult(){
+    // call this func  when equal button is pressed
+    previousNumberValue=Number(previousNumberValue);
+    currentNumberValue=Number(currentNumberValue);
+    
+    if (operatorValue==="+"){
+         previousNumberValue += currentNumberValue
+    }else if(operatorValue==="-"){
+        previousNumberValue -= currentNumberValue     
+    }
+    else if(operatorValue==="*"){
+        previousNumberValue *= currentNumberValue     
+    }
+    else if(operatorValue==="/"){
+         previousNumberValue /= currentNumberValue     
+    }
+    previousNumberValue=previousNumberValue.toString();
+    currentNumberValue=previousNumberValue.toString();
     
 }
